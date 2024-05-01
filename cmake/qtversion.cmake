7d5112477202da1ccfab5e06642935bd9bf0f0d2
Yinan Qin <39023210+ganyuanzhen@users.noreply.github.com>
2024-05-01 03:55:41 +0000

    Fix CMake

--- cmake/qtversion.cmake
+++ cmake/qtversion.cmake
@@ -8,7 +8,9 @@ set(CMAKE_MODULE_PATH
         ${ECM_MODULE_PATH}
         ${ECM_KDE_MODULE_DIR}
 )
-if(ECM_VERSION VERSION_GREATER "5.200.0")
-    set(QT_MIN_VERSION "6.4.0")
-    set(QT_MAJOR_VERSION 6)
-endif()
+
+# Currently not support Qt6 in our sys
+#if(ECM_VERSION VERSION_GREATER "5.200.0")
+#    set(QT_MIN_VERSION "6.4.0")
+#    set(QT_MAJOR_VERSION 6)
+#endif()
